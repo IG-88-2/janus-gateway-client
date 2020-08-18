983eeea761b532d4c590043ffc605e65c8c49050
IG-88-2 <anatoly.strashkevich@gmail.com>
2020-08-18 10:46:57 +0300

    dev

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,30 +3,13 @@ import commonjs from 'rollup-plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
 import copy from 'rollup-plugin-copy';
-const pkg = {
-  "main": "dist/index.cjs.js",
-  "module": "dist/index.esm.js",
-  "browser": "dist/index.js"
-};
 
 export default {
   input: `janus-gateway-client.ts`,
   output: [
     {
       name: 'janus-gateway-client', 
-      file: pkg.browser,
-      format: 'umd',
-      sourcemap: false
-    },
-    {
-      name: 'janus-gateway-client', 
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: false
-    },
-    {
-      name: 'janus-gateway-client', 
-      file: pkg.module,
+      file: "dist/index.js",
       format: 'es',
       sourcemap: false
     }
@@ -39,7 +22,10 @@ export default {
     sourceMaps(),
     copy({
       targets: [
-        { src: './package.json', dest: 'dist' }
+        { 
+          src: './package.json', 
+          dest: 'dist' 
+        }
       ]
     })
   ]
